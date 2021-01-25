931c15b9d4001aecfca28d436868d2e89a55048a
DawDab <48210609+DawDab@users.noreply.github.com>
2021-01-26 00:23:27 +0100

    Update mma8451q.c

--- mma8451q.c
+++ mma8451q.c
@@ -30,44 +30,44 @@ void Mma8451q_Init(void)
 	NVIC_EnableIRQ(30);
 	NVIC_SetPriority(30, 4);
 	
-	I2C_WriteReg(REG_WHO_AM_I, CTRL_REG1,0x10); //200 Hz, Standby Mode
-	I2C_WriteReg(REG_WHO_AM_I, CTRL_REG2, 0x02);
+	I2C_WriteReg(ACC_ADDR, CTRL_REG1,0x10); //200 Hz, Standby Mode
+	I2C_WriteReg(ACC_ADDR, CTRL_REG2, 0x02);
 	
 	//Step 2: Enable X, Y, Z Single Pulse and X, Y and Z Double Pulse with DPA = 0 no double pulse abort
-	I2C_WriteReg(0x1D, PULSE_CFG, 0x3F);
+	I2C_WriteReg(ACC_ADDR, PULSE_CFG, 0x3F);
 
 	//Step 3: Set Threshold 2g on X and Y and 4g on Z
 	//Note: Every step is 0.063g
 	//2g / 0.063g = 32 counts
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_THSX, 0x64); //Set X Threshold 
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_THSY, 0x64); //Set Y Threshold 
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_THSZ, 0x64); //Set Z Threshold 
+	I2C_WriteReg(ACC_ADDR, PULSE_THSX, 0x64); //Set X Threshold 
+	I2C_WriteReg(ACC_ADDR, PULSE_THSY, 0x64); //Set Y Threshold 
+	I2C_WriteReg(ACC_ADDR, PULSE_THSZ, 0x64); //Set Z Threshold 
 
 	//Step 4: Set Time Limit for Tap Detection to 60 ms (LP Mode, 200 Hz ODR, No LPF)
 	//Note: 200 Hz ODR LP Mode, Time step is 2.5 ms per step
 	//60 ms /2.5 ms = 24 counts
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_TMLT,0x18); 
+	I2C_WriteReg(ACC_ADDR, PULSE_TMLT,0x18); 
 
 	//Step 5: Set Latency Timer to 200 ms
 	//Note: 200 Hz ODR LP Mode, Time step is 5 ms per step
 	//200 ms/ 5 ms = 40 counts
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_LTCY,0x50);  
+	I2C_WriteReg(ACC_ADDR, PULSE_LTCY,0x50);  
 
 	//Step 6: Set Time Window for Second Tap to 300 ms
 	//Note: 200 Hz ODR LP Mode, Time step is 5 ms per step
 	//300 ms/5 ms = 60 counts
-	I2C_WriteReg(REG_WHO_AM_I, PULSE_WIND,0x3C); 
+	I2C_WriteReg(ACC_ADDR, PULSE_WIND,0x3C); 
 
 	//Step 7: Route INT1 to System Interrupt
-	I2C_WriteReg(REG_WHO_AM_I, CTRL_REG4, 0x08); //Enable Pulse Interrupt Block in System CTRL_REG4
-	I2C_WriteReg(REG_WHO_AM_I, CTRL_REG5, 0xF7); //Route Pulse Interrupt Block to INT1 hardware Pin // PTA10/IRQ12
+	I2C_WriteReg(ACC_ADDR, CTRL_REG4, 0x08); //Enable Pulse Interrupt Block in System CTRL_REG4
+	I2C_WriteReg(ACC_ADDR, CTRL_REG5, 0xF7); //Route Pulse Interrupt Block to INT1 hardware Pin // PTA10/IRQ12
 
 	//Step 8: Active Mode
 	//Read out the contents of the register
 	//Change the value in the register to Active Mode.
-	I2C_ReadReg (REG_WHO_AM_I,  CTRL_REG1, &ctrl_reg1_data);
+	I2C_ReadReg (ACC_ADDR,  CTRL_REG1, &ctrl_reg1_data);
 	ctrl_reg1_data|= 0x01;
-	I2C_WriteReg(REG_WHO_AM_I,CTRL_REG1, ctrl_reg1_data);
+	I2C_WriteReg(ACC_ADDR,CTRL_REG1, ctrl_reg1_data);
 
 	UART_Println("\n\r ~~~~ TAP DETECTION ON: ~~~~ ");
 }
